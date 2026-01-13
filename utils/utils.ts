76351b90ac11e9c5fce81340e079118bb613d257
SeaotterMS <peter910820@gmail.com>
2026-01-13 13:39:41 +0800

    chore: remove unuse field

--- utils/utils.ts
+++ utils/utils.ts
@@ -5,11 +5,8 @@ export const flattenUserGameErogs = (data: UserGameErogs): UserGameErogsFlat => 
   return {
     userId: data.userId,
     gameErogsId: data.gameErogsId,
-    hasPlayed: data.hasPlayed,
-    inWish: data.inWish,
     completedAt: data.completedAt,
     createdAt: data.createdAt,
-    updatedAt: data.updatedAt,
     gameName: data.gameErogs.name,
     brandId: data.gameErogs.brandErogs.id,
     brandName: data.gameErogs.brandErogs.name,
